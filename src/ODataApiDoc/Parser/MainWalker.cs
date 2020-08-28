0259b0059ffcaf111e0e1a1324f98b6df94a7c4d
kavics <gyeby5@gmail.com>
2020-08-28 06:55:08 +0200

    Parser refactored.

---
+++ src/ODataApiDoc/Parser/MainWalker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ODataApiDoc.Parser
+{
+    /// <summary>
+    /// Searches ODataAction or ODataFunction attributes and visits their methods
+    /// </summary>
+    internal class MainWalker : WalkerBase
+    {
+        public List<OperationInfo> Operations { get; } = new List<OperationInfo>();
+
+        private readonly string _path;
+
+        public MainWalker(string path, bool showAst) : base(showAst)
+        {
+            _path = path;
+        }
+
+        public override void VisitAttribute(AttributeSyntax node)
+        {
+            var name = node.Name.ToString();
+            if (name == "ODataFunction" || name == "ODataAction")
+            {
+                // MethodDeclarationSyntax -> AttributeListSyntax -> AttributeSyntax
+                var walker = new ODataOperationWalker(this.ShowAst);
+                using (Color(ConsoleColor.Cyan))
+                    walker.Visit(node.Parent.Parent);
+
+                var op = walker.Operation;
+                op.File = _path;
+
+                GetNamespaceAndClassName(node, out var @namespace, out var className);
+                op.Namespace = @namespace;
+                op.ClassName = className;
+
+                op.Normalize();
+                Operations.Add(op);
+            }
+            else
+            {
+                base.VisitAttribute(node);
+            }
+        }
+
+        private void GetNamespaceAndClassName(AttributeSyntax node, out string @namespace, out string className)
+        {
+            ClassDeclarationSyntax classNode;
+            NamespaceDeclarationSyntax namespaceNode;
+            SyntaxNode n = node;
+            while ((classNode = n as ClassDeclarationSyntax) == null)
+                n = n.Parent;
+            while ((namespaceNode = n as NamespaceDeclarationSyntax) == null)
+                n = n.Parent;
+
+            @namespace = namespaceNode.Name.ToString();
+            className = classNode.Identifier.ToString();
+        }
+    }
+}
---
+++ src/ODataApiDoc/Parser/OperationParser.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace ODataApiDoc.Parser
+{
+    internal class OperationParser
+    {
+        private Options _options;
+
+        public OperationParser(Options options)
+        {
+            _options = options;
+        }
+
+        public List<OperationInfo> Parse()
+        {
+            var operations = new List<OperationInfo>();
+            var input = Path.GetFullPath(_options.Input);
+            //string rootPath;
+            if (File.Exists(input))
+            {
+                if (Path.GetExtension(input) != ".cs")
+                    throw new NotSupportedException("Only csharp file (*.cs extension) is supported.");
+                //rootPath = Path.GetDirectoryName(input);
+                AddOperationsFromFile(input, input, operations, null, _options.ShowAst);
+            }
+            else
+            {
+                if (!Directory.Exists(input))
+                    throw new ArgumentException("Unknown file or directory: " + input);
+                //rootPath = input.TrimEnd('\\', '/');
+                AddOperationsFromDirectory(input, input, operations, null, _options.ShowAst);
+            }
+
+            return operations;
+        }
+
+        private void AddOperationsFromDirectory(string root, string path, List<OperationInfo> operations,
+            ProjectInfo currentProject, bool showAst)
+        {
+            if (path.EndsWith("\\obj", StringComparison.OrdinalIgnoreCase))
+                return;
+
+            var projectPath = Directory.GetFiles(path, "*.csproj").FirstOrDefault();
+            if (projectPath != null)
+                currentProject = CreateProject(projectPath);
+
+            foreach (var directory in Directory.GetDirectories(path))
+                AddOperationsFromDirectory(root, directory, operations, currentProject, showAst);
+            foreach (var file in Directory.GetFiles(path, "*.cs"))
+                AddOperationsFromFile(root, file, operations, currentProject, showAst);
+        }
+
+        private ProjectInfo CreateProject(string projectPath)
+        {
+            var path = Path.GetDirectoryName(projectPath);
+            var name = Path.GetFileNameWithoutExtension(projectPath);
+            var typeName = GetProjectTypeName(projectPath);
+            var type = GetProjectType(typeName);
+            var isTest = name.EndsWith("test", StringComparison.OrdinalIgnoreCase) ||
+                         name.EndsWith("tests", StringComparison.OrdinalIgnoreCase);
+
+            return new ProjectInfo
+            {
+                Path = path,
+                Name = name,
+                TypeName = typeName,
+                Type = type,
+                IsTestProject = isTest
+            };
+        }
+
+        private string GetProjectTypeName(string projectPath)
+        {
+            var src = File.ReadAllLines(projectPath).Select(x => x.Trim()).ToArray();
+            var targetFwLine = src.FirstOrDefault(x => x.StartsWith("<TargetFramework>"));
+            if (targetFwLine != null)
+            {
+                var typeName = targetFwLine
+                    .Replace("</TargetFramework>", "", StringComparison.OrdinalIgnoreCase)
+                    .Replace("<TargetFramework>", "", StringComparison.OrdinalIgnoreCase);
+                return typeName;
+            }
+
+            var targetFwVersionLine = src.FirstOrDefault(x => x.StartsWith("<TargetFrameworkVersion>"));
+            if (targetFwVersionLine != null)
+            {
+                var version = targetFwVersionLine
+                    .Replace("</TargetFrameworkVersion>", "", StringComparison.OrdinalIgnoreCase)
+                    .Replace("<TargetFrameworkVersion>v", "", StringComparison.OrdinalIgnoreCase)
+                    .Replace("<TargetFrameworkVersion>", "", StringComparison.OrdinalIgnoreCase);
+                return "netframework" + version;
+            }
+
+            throw new ApplicationException("Unknown project: " + projectPath);
+        }
+
+        private ProjectType GetProjectType(string typeName)
+        {
+            if (typeName.StartsWith("netcoreapp"))
+                return ProjectType.NETCore;
+            if (typeName.StartsWith("netstandard"))
+                return ProjectType.NETStandard;
+            if (typeName.StartsWith("netframework"))
+                return ProjectType.NETFramework;
+            return ProjectType.Unknown;
+        }
+
+        private void AddOperationsFromFile(string root, string path, List<OperationInfo> operations,
+            ProjectInfo currentProject, bool showAst)
+        {
+            if (path.Length > root.Length)
+            {
+                var line = path.Substring(root.Length + 1).PadRight(Console.BufferWidth - 1);
+                Console.Write(line + "\r");
+            }
+
+            var code = new StreamReader(path).ReadToEnd();
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var walker = new MainWalker(path, showAst);
+            walker.Visit(tree.GetRoot());
+
+            foreach (var op in walker.Operations)
+                op.Project = currentProject;
+
+            operations.AddRange(walker.Operations);
+
+            if (walker.Operations.Count > 0)
+            {
+                Console.WriteLine("{0}: {1}", path, walker.Operations.Count);
+            }
+        }
+
+    }
+}
